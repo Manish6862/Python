d42721c96faf13ef84dc41e0dfc83e6c9a53a2d9
Manish Rajput <manishrajput67321@gmail.com>
2025-06-03 19:12:48 +0530

    Operators

---
+++ operators.py
@@ -0,0 +1,22 @@
+#Assignment Operators
+
+# a = 7
+# b = 4
+# print( a + b )
+# print( a - b )
+# print( a * b )
+# print( a / b )
+# print( a % b ) remainder
+# print( a ** b ) a^b
+
+
+#Relational Operators
+
+a = 50 
+b = 30
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
