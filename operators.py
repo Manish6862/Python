d197970c6c1533e1841f0edd71b9aea0cfce46e0
Manish Rajput <manishrajput67321@gmail.com>
2025-06-03 22:49:23 +0530

    Operators

--- operators.py
+++ operators.py
@@ -1,4 +1,4 @@
-#Assignment Operators
+#Arthmatic Operators
 
 # a = 7
 # b = 4
@@ -11,12 +11,79 @@
 
 
 #Relational Operators
+# a = 50 
+# b = 30
+# print(a == b)
+# print(a != b)
+# print(a < b)
+# print(a > b)
+# print(a <= b)
+# print(a >= b)
 
-a = 50 
-b = 30
-print(a == b)
-print(a != b)
-print(a < b)
-print(a > b)
-print(a <= b)
-print(a >= b)
+#Assignment Operators
+
+# num = 10
+# print("num: ", num)
+
+# # num = num + 10
+# num += 10
+# print("num: ", num)
+
+# num1 = 30
+# # num1 = num1 - 10
+# num1 -= 15
+# print("num1 : " ,num1)
+
+# num2 = 20
+# # num2 = num2 * 10
+# num2 *= 100
+# print("num2: ", num2)
+
+# num3 = 15
+# # num3 = num3 / 5
+# num3 /= 5
+# print("num3: ", num3)
+
+# num4 = 7
+# # num4 = num4 % 4
+# num4 %= 4
+# print("Num4: ", num4)
+
+# num5 = 8
+# # num5 = num5 ** 2
+# num5 **= 2
+# print("Num 5 : ", num5)
+
+
+## Logical Operators
+
+# #not
+# print(not False)
+# print(not True)
+# a = 20
+# b = 20
+# print(not (a == b))
+# c = 50
+# d = 30
+# print(not (c < d))
+
+#and
+val1 = True
+val2 = True
+print("And ans :",val1 and val2)
+
+val1 = True
+val2 = False
+print("And Operator :",val1 and val2)
+
+a = 30
+b = 20
+print("And Operator :", (a < b) and (a >= b))
+
+val1 = True
+val2 = False
+print("or Operator :",val1 or val2)
+
+a = 30
+b = 20
+print("And Operator :", (a > b) or (a >= b))
