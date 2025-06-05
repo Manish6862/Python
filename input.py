079873d69423e00aa7406cd88fc3b0b4ece826c3
Manish Rajput <manishrajput67321@gmail.com>
2025-06-05 19:29:05 +0530

    Input

---
+++ input.py
@@ -0,0 +1,48 @@
+# name = input("Enter Your Name: ")
+# print(type(name), name)
+
+
+# age = int(input("Enter YOur Age :"))
+# print(type(age) , age)
+
+
+# #Take Input Your Name,Marks,Age
+# name = input("Enter Your Name : ")
+# marks = float(input("Enter your Marks : "))
+# age = int(input("Enter Your age : "))
+# print("Your name: ", name)
+# print("Your marks: ",marks)
+# print("Your age : ",age)
+
+
+
+# #Question 1
+# #Take input 2 number and print their sum
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# sum = a+b
+# print("Sum: ", sum)
+
+
+#Question 2 
+# Input side os a square and print its area
+
+# side = float(input("Enter side of Square: "))
+# area = side *side
+# print("Area of Square: ")
+
+#Question 3
+#Input two floating number and print its avarage
+
+# a = float(input("Enter first number: "))
+# b = float(input("Enter second number: "))
+# avg = (a + b)/2
+# print("Avg: ",avg)
+
+#Question 4
+#Input Two int number A and B and Print ture A is greater than or qual to B.
+#If not Print False
+
+# a = int(input("Enter Value Of A: "))
+# b = int(input("Enter Value Of B: "))
+# print( a>= b)
